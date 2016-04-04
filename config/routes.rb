5d2b410ee3e1d3efb9d5b8fc43b29f7b43517ebb
Ben Morris <git@benmorris.net>
2016-04-04 17:52:58 -0400

    add routes for app/build & redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  root to: redirect('/app/build/')
+  get '/app/assets/:folder/:file.:ext', to: redirect('/app/build/assets/%{folder}/%{file}.%{ext}')
+  get '/app/templates/:file.html', to: redirect('/app/build/templates/%{file}.html')
+
+
   namespace :api do 
     namespace :v1 do 
       resources :schemas, only:[:index,:show] 
