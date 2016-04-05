ffb1c91f9cf6a3b23971d72550a9b3508536c011
Ben Morris <git@benmorris.net>
2016-04-05 09:32:32 -0400

    minor cosmetic & comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  # Static Files
   root to: redirect('/app/build/')
   get '/app/assets/:folder/:file.:ext', to: redirect('/app/build/assets/%{folder}/%{file}.%{ext}')
   get '/app/templates/:file.html', to: redirect('/app/build/templates/%{file}.html')
 
-
+  # Core APIs
   namespace :api do 
     namespace :v1 do 
       resources :schemas, only:[:index,:show]
