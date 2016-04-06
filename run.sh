e9653d72850b9f3b5cdf890041acaec9fa4ae44a
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2016-04-06 15:43:04 -0400

    Fix AWS deploy

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# let the DB launch
+sleep 10
+
+# build the DB
+bundle exec rake db:create db:migrate db:seed
+
+# run gulp & rails in parallel
 gulp --cwd $STATIC &
 bundle exec rails server -b 0.0.0.0
-
 wait
